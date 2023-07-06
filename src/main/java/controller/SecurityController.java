d1fbec680094d746c2102d23646c5fa7136df437
Felix Karani Maina <fkarani@hq.ekenya.co.ke>
2023-07-06 09:34:59 +0300

    add spring security dependency

--- src/main/java/controller/SecurityController.java
+++ src/main/java/controller/SecurityController.java
@@ -1,2 +1,13 @@
-package controller;public class SecurityController {
+package controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @GetMapping("welcome")
+    ResponseEntity<String>welcome(){
+        return ResponseEntity.ok("welcome");
+    }
 }
